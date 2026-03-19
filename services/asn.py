2acafcc08c72efd93898b5d1314e191c9727a81e
nameruse2 <nameruse@protonmail.com>
2026-03-19 22:33:52 +0000

    added initial team cymru ASN lookup function

---
+++ services/asn.py
@@ -0,0 +1,38 @@
+import dns.resolver
+import ipaddress
+
+def ip_version(ip: str) -> int:
+    ipaddress.ip_address(ip).version
+
+def cymru_ipv4_to_asn(ip: str) -> str:
+    reversed_ip = ".".join(reversed(ip.split(".")))
+    query = f"{reversed_ip}.origin.asn.cymru.com"
+    answer = dns.resolver.resolve(query, "TXT")[0]
+    txt = answer.to_text().strip('"')
+    parts = txt.split(" | ")
+    return f"AS{parts[0]}"
+
+def cymru_ipv6_to_asn(ip: str):
+    pass
+
+
+def cymru_asn(asn):
+    query = f"{asn}.asn.cymru.com"
+    answer = dns.resolver.resolve(query, "TXT")[0]
+    txt = answer.to_text().strip('"')
+    parts = txt.split(" | ")
+
+    return {
+        "asn": int(parts[0]),
+        "asn_country": parts[1],
+        "asn_registry": parts[2],
+        "asn_date": parts[3],
+        "asn_allocated": parts[4],
+    }
+
+def cymru(ip):
+    if ipaddress.ip_address(ip).version == 4:
+        asn = cymru_ipv4_to_asn(ip)
+    else:
+        asn = cymru_ipv6_to_asn(ip)
+    return cymru_asn(asn)
