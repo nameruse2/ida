b4bcd35694b171c74aa2856dd93b18309198b149
nameruse2 <nameruse@protonmail.com>
2026-03-21 23:38:52 +0000

    Updated DNS resolver to use 1.1.1.1
    Update DNS resolver to use Cloudflare 1.1.1.1 DNS
    Added typehints

--- services/dns.py
+++ services/dns.py
@@ -1,15 +1,17 @@
 import dns.resolver
 
 
-def _query(domain, record_type):
+def _query(domain: str, record_type: str) -> list:
     try:
-        answers = dns.resolver.resolve(domain, record_type)
+        resolver = dns.resolver.Resolver()
+        resolver.nameservers: list[str] = ['1.1.1.1']
+        answers = resolver.resolve(domain, record_type)
         return [r.to_text() for r in answers]
     except Exception:
         return []
 
 
-def dns_lookup(domain):
+def dns_lookup(domain: str) -> dict:
     return {
         "A": _query(domain, "A"),
         "AAAA": _query(domain, "AAAA"),
